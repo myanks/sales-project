7335894c6be5c3fa1e89d0fa4ad8577e8a804e97
Mayank Singh <mayanksingh56643@gmail.com>
2024-12-28 20:10:47 +0530

    Adding bootstrap to the page

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,13 +3,14 @@ import { SalesPerson } from './sales-person';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrl: './sales-person-list.component.css',
   imports: [CommonModule],
 })
 export class SalesPersonListComponent implements OnInit {
+  targetedQuota: number = 5000;
   SalesPersonList: SalesPerson[] = [
-    new SalesPerson('John', 'Doe', 'john.doe@example.com', 500),
+    new SalesPerson('John', 'Doe', 'john.doe@example.com', 5000),
     new SalesPerson('Jane', 'Doe', 'jane.doe@example.com', 4000),
     new SalesPerson('John', 'Doe', 'john.doe@example.com', 6000),
     new SalesPerson('Jane', 'Doe', 'jane.doe@example.com', 7000),
